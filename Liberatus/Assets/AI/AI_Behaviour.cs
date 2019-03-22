2e8b599b541fb49b98899d37b816bbb95ca7d60a
Charles <charlesanz45@gmail.com>
2019-03-22 22:46:18 +0000

    AI development

--- Liberatus/Assets/AI/AI_Behaviour.cs
+++ Liberatus/Assets/AI/AI_Behaviour.cs
@@ -89,7 +89,7 @@ public class AI_Behaviour : MonoBehaviour
     {
        if( collision.gameObject.tag == "Player")
        {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
        }
     }
 }
